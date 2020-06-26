6dc6adb29e0740cbf5bbed2819e2c58a6dd068cd
Vazquez Cesar <cesar.evc@outlook.com>
2020-06-26 14:43:00 -0500

    contry picker fetch api by country and update charts

--- src/components/CountryPicker/CountryPicker.jsx
+++ src/components/CountryPicker/CountryPicker.jsx
@@ -1,7 +1,31 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+// material resources
+import { NativeSelect, FormControl } from '@material-ui/core';
+// fetch api 
+import { fetchCountries } from '../../api';
+// css styles
+import styles from './CountryPicker.module.css';
 
-function CountryPicker () {
-    return <h1>CountryPicker</h1>
+function CountryPicker ({ handleCountryChange }) {
+
+    const [countries, setCountries ] = useState([]);
+
+    useEffect(() => {
+        const fetchAPI = async () => {
+            setCountries(await fetchCountries());
+        }
+
+        fetchAPI();
+    }, [])
+
+    return (
+        <FormControl className={styles.formControl}>
+            <NativeSelect defaultValue="" onChange={e => handleCountryChange(e.target.value)}>
+                <option value="">Global</option>
+                {countries.map((country, i) => <option value={country} key={i}>{country}</option> )}
+            </NativeSelect>
+        </FormControl>
+    )
 }
 
 export default CountryPicker;
