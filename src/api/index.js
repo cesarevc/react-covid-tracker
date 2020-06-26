667643ece332be043954e447ccc201f4edc5c003
Vazquez Cesar <cesar.evc@outlook.com>
2020-06-26 14:40:00 -0500

    fetch api

--- src/api/index.js
+++ src/api/index.js
@@ -3,20 +3,49 @@ import axios from 'axios';
 const url = 'https://covid19.mathdro.id/api';
 
 
-export const fetchApi = async () => {
+export const fetchApi = async (country) => {
+    let changeableURL = url
+
+    if (country) {
+        changeableURL = `${url}/countries/${country}`;
+    }
+
     try {
-        const { data: {confirmed, recovered, deaths, lastUpdate} } = await axios.get(url);
+        const { data: {confirmed, recovered, deaths, lastUpdate} } = await axios.get(changeableURL);
 
-        const processedData = {
-            confirmed,
-            recovered,
-            deaths,
-            lastUpdate
-        }
+        return { confirmed, recovered, deaths, lastUpdate };
 
-        return processedData;
-        // console.log(response)
-    } catch (e) {
-        
+    } catch (err) {
+        return err;
     }
 } 
+
+
+export const fetchDailyData = async () => {
+    try {
+        const { data } = await axios.get(`${url}/daily`);
+
+        const processedData = data.map((dailyData) => ({
+            confirmed: dailyData.confirmed.total,
+            deaths: dailyData.deaths.total,
+            date: dailyData.reportDate
+        }))
+
+        return processedData;
+
+    } catch (err) {
+        return err;
+    }
+}
+
+
+export const fetchCountries = async () => {
+    try {
+        const { data: { countries }} = await axios.get(`${url}/countries`);
+
+        return countries.map(country => country.name);
+
+    } catch (err) {
+        return err;
+    }
+}
